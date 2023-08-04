639dbc89622e888e2cca122313211685615bd27c
Ahkar Hein <ahkarhein04@gmail.com>
2023-08-04 16:39:28 -0700

    added comment in server.js and updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Employee-Tracker
+## Description 
+The Employee Management System is a command-line application that allows users to manage departments, roles, and employees of a company. It provides a simple and user-friendly interface for performing various tasks related to employee management. This application used mysql for store the employee, department and role data and inquirer package for user input.
+
+## Table of Contents
+            
+* [Installation](#installation)
+* [Usage](#usage)
+* [Walkthrough-Video](#walkthrough-video)
+* [Contributing](#contributing)
+* [Question](#question)
+            
+## Installation
+Please read the installation instruction are as follow:
+
+* Execute the schema.sql and seed.sql from db folder.
+* run npm i in your terminal.
+* Then, you can start your application by running npm start.
+ 
+            
+## Usage 
+Clone the repo and run the application with installation instruction.
+
+## Walkthrough-Video
+
+You can view the walkthrough video at [https://drive.google.com/file/d/1rkM7fwQPvPhyjn-jaiKVD0Zt_q1NlW4f/view].
+            
+## Contributing
+Fork the repo and copy it into your GitHub.
+            
+## Question
+If you have any questions about this repo, contact me directly at ahkarhein04@gmail.com. You can find more my work at (https://github.com/ahkar-hein)
--- server.js
+++ server.js
@@ -20,26 +20,35 @@ function queryAsync(sql, params) {
         });
     });
 }
-
+// Create async function for view all department.
 async function viewDepartments() {
+    // try/Catch condition for sql query.
     try {
-        const results = await queryAsync('SELECT id as ID, name as Department FROM department');
+        const results = await queryAsync('SELECT id as ID, name as Department FROM department'); //Select id and name from department table.
 
+        // Format the data without the index column
         const formattedResults = results.map(({ ID, ...rest }) => ({ ID: ID, ...rest }));
+        // Create a new table without the index column
         const table = new Table({ head: Object.keys(formattedResults[0]), style: { 'padding-left': 0, 'padding-right': 0 } });
-
+        // Push the rows into the table
         formattedResults.forEach((row) => {
             table.push(Object.values(row));
         });
+        // Display the table
         console.log(table.toString());
         mainMenu();
     } catch (err) {
+        // display the error if catch error
         console.error('Error while fetching departments:', err);
+        // execute the mainMenu function
         mainMenu();
     }
 }
+// Async function for view all roles
 async function viewRoles() {
     try {
+        // SQL query for select id, title, salary and department name from role and make a inner join with department
+        // Then make an on condition with department id from role is equal to id from department table.
         const results = await queryAsync(`SELECT r.id as ID, r.title as Title, r.salary as Salary, d.name as Department  FROM role as r
         INNER JOIN department as d
         on r.department_id = d.id;`);
@@ -57,9 +66,12 @@ async function viewRoles() {
         mainMenu();
     }
 }
-
+// Async function for view all employees
 async function viewEmployees() {
     try {
+        // This query work select everything from employee, title and salary from role table and department name from department table.
+        // Then, Make a self join for employee table to get manager id and name for employee who have manager and left join for role and department table
+        // to get title, salary and department name.
         const results = await queryAsync(`
         SELECT emp.id AS Employee_id, emp.first_name AS First_Name, emp.last_name AS Last_Name, 
         role.title AS Title, dept.name AS Department, role.salary AS Salary, manager.first_name AS Manager 
@@ -68,23 +80,24 @@ async function viewEmployees() {
         LEFT JOIN role ON emp.role_id = role.id 
         LEFT JOIN department AS dept ON role.department_id = dept.id
       `);
-      const formattedResults = results.map(({ Employee_id, ...rest }) => ({ Emplyoyee_ID: Employee_id, ...rest }));
-      const table = new Table({ head: Object.keys(formattedResults[0]), style: { 'padding-left': 0, 'padding-right': 0 } });
+        const formattedResults = results.map(({ Employee_id, ...rest }) => ({ Emplyoyee_ID: Employee_id, ...rest }));
+        const table = new Table({ head: Object.keys(formattedResults[0]), style: { 'padding-left': 0, 'padding-right': 0 } });
 
-      formattedResults.forEach((row) => {
-          table.push(Object.values(row));
-      });
-      console.log(table.toString());
-        
+        formattedResults.forEach((row) => {
+            table.push(Object.values(row));
+        });
+        console.log(table.toString());
+
         mainMenu();
     } catch (err) {
         console.error('Error while fetching employees:', err);
         mainMenu();
     }
 }
-
+// Async function for add new department
 async function addDepartment() {
     try {
+        // Create the inquirer prompt for add new department
         const answers = await inquirer.prompt([
             {
                 name: 'name',
@@ -92,6 +105,7 @@ async function addDepartment() {
                 message: 'Enter the name of the department:',
             },
         ]);
+        // this query for insert new department name into department table. ? mean there is a variables where ? placed.
         await queryAsync('INSERT INTO department (name) VALUES (?)', [answers.name]);
         console.log('Department added successfully!');
         mainMenu();
@@ -100,10 +114,12 @@ async function addDepartment() {
         mainMenu();
     }
 }
-
+// This is async function for add new role
 async function addRole() {
     try {
+        // First, getting id and name from department table for user choice when adding the new role.
         const departments = await queryAsync('SELECT id, name FROM department');
+        // inquirer prompt for add new role
         const answers = await inquirer.prompt([
             {
                 name: 'title',
@@ -119,9 +135,11 @@ async function addRole() {
                 name: 'department_id',
                 type: 'list',
                 message: 'Choose the department for the role:',
+                // This code transforms an array of department objects into a new array of objects with 'name' and 'value' properties.
                 choices: departments.map((department) => ({ name: department.name, value: department.id })),
             },
         ]);
+        // Then, insert query that inserting title, salary and department id into role table.
         await queryAsync('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [
             answers.title,
             answers.salary,
@@ -134,12 +152,14 @@ async function addRole() {
         mainMenu();
     }
 }
-
+// This is async functon for addEmployee
 async function addEmployee() {
     try {
+        // first, getting id, title from role table.
         const roles = await queryAsync('SELECT id, title FROM role');
+        // Then, id, first_name, last_name from employee table for employee who has manager.
         const employees = await queryAsync('SELECT id, first_name, last_name FROM employee');
-        employees.unshift({ id: null, first_name: 'None', last_name: '' });
+        // inquirer prompt for add new employee
         const answers = await inquirer.prompt([
             {
                 name: 'first_name',
@@ -162,11 +182,13 @@ async function addEmployee() {
                 type: 'list',
                 message: 'Choose the manager for the employee:',
                 choices: employees.map((employee) => ({
+                    // if employee doesn't have manager, manager id insert as null, otherwise, employee id is going to be inserted.
                     name: employee.id === null ? 'No Manager' : `${employee.first_name} ${employee.last_name}`,
                     value: employee.id,
                 })),
             },
         ]);
+        // Insert query for new employee
         await queryAsync('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [
             answers.first_name,
             answers.last_name,
@@ -180,16 +202,20 @@ async function addEmployee() {
         mainMenu();
     }
 }
-
+// Async function for upadate Employee role.
 async function updateEmployeeRole() {
     try {
+        // getting id, firstname, lastname from employee
         const employees = await queryAsync('SELECT id, first_name, last_name FROM employee');
+        // getting id, title from role
         const roles = await queryAsync('SELECT id, title FROM role');
+        // prompt for update employee role
         const answers = await inquirer.prompt([
             {
                 name: 'employee_id',
                 type: 'list',
                 message: 'Choose the employee to update:',
+                // display employee name list that come from database by using map methods.
                 choices: employees.map((employee) => ({
                     name: `${employee.first_name} ${employee.last_name}`,
                     value: employee.id,
@@ -199,9 +225,11 @@ async function updateEmployeeRole() {
                 name: 'new_role_id',
                 type: 'list',
                 message: 'Choose the new role for the employee:',
+                // display role title list that come from database by using map methods.
                 choices: roles.map((role) => ({ name: role.title, value: role.id })),
             },
         ]);
+        // Update query for new employee
         await queryAsync('UPDATE employee SET role_id = ? WHERE id = ?', [answers.new_role_id, answers.employee_id]);
         console.log('Employee role updated successfully!');
         mainMenu();
@@ -210,8 +238,9 @@ async function updateEmployeeRole() {
         mainMenu();
     }
 }
-
+//function for mainMenu
 function mainMenu() {
+    // Prompt the mainMenu list and make a user choice
     inquirer
         .prompt([
             {
@@ -231,6 +260,7 @@ function mainMenu() {
             },
         ])
         .then((answers) => {
+            // switch case conditon and execute the relevant function that based on the user choice.
             switch (answers.action) {
                 case 'View all departments':
                     viewDepartments();
@@ -243,6 +273,7 @@ function mainMenu() {
                     break;
                 case 'Add a department':
                     addDepartment();
+                    break;
                 case 'Add a role':
                     addRole();
                     break;
@@ -268,6 +299,22 @@ db.connect((err) => {
         console.error('Error connecting to the database:', err);
         return;
     }
-    console.log('Connected to the database.');
+    console.log(`
+    #######  ##   ##  ######   ####      #####   ###  ### #######  #######  
+    ##   #  ### ###   ##  ##   ##      ### ###   ##  ##   ##   #   ##   #  
+    ##      #######   ##  ##   ##      ##   ##    ####    ##       ##      
+    ####    ## # ##   #####    ##      ##   ##     ##     ####     ####    
+    ##      ##   ##   ##       ##      ##   ##     ##     ##       ##      
+    ##   #  ##   ##   ##       ##  ##  ### ###     ##     ##   #   ##   #  
+   #######  ### ###  ####     #######   #####     ####   #######  #######  
+                                                                           
+    # ##### ######     ###      ####   ### ###  #######  ######   
+   ## ## ##  ##  ##   ## ##    ##  ##   ## ##    ##   #   ##  ##  
+      ##     ##  ##  ##   ##  ##        ####     ##       ##  ##  
+      ##     #####   ##   ##  ##        ###      ####     #####   
+      ##     ## ##   #######  ##        ####     ##       ## ##   
+      ##     ## ##   ##   ##   ##  ##   ## ##    ##   #   ## ##   
+     ####   #### ##  ##   ##    ####   ### ###  #######  #### ##              
+`);
     mainMenu();
 });
