fd44a5411e20c624c33a6f56fa0f7489988f7b22
Ahkar Hein <ahkarhein04@gmail.com>
2023-08-03 22:35:45 -0700

    Create function for mainMenu

--- server.js
+++ server.js
@@ -10,6 +10,27 @@ const db = mysql.createConnection({
     database: 'employee_tracker_db',
 });
 
+function mainMenu() {
+    inquirer
+        .prompt([
+            {
+                name: 'action',
+                type: 'list',
+                message: 'Choose an option:',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role',
+                    'Exit',
+                ],
+            },
+        ])
+        //Will add switch case later
+}
 
 // Connect to the database and start the application
 db.connect((err) => {
